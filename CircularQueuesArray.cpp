6e6496dadbb76ff32c220e17f3e524c17d7b4b65
annisafathul <annisafathul18@gmail.com>
2026-05-28 15:09:15 +0700

    Menambahkan circular front

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -57,8 +57,13 @@ class Queues
             FRONT = -1;
             REAR = -1;
         }
-
-
+        else
+        {
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
 
 };
