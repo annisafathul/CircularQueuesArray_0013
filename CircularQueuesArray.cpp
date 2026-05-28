01f49a881ac1a0f3d409fe8ad2d4411e33a88b58
annisafathul <annisafathul18@gmail.com>
2026-05-28 15:16:25 +0700

    Menambahkan perulangan circular queue

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -87,9 +87,15 @@ class Queues
             }
             cout << endl;
         }
-
-
-
+        else 
+        {
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
     }
 
 };
