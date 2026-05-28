a6ed3b51026a7742d89f828d2d6b0e5c921bdcc5
annisafathul <annisafathul18@gmail.com>
2026-05-28 15:01:50 +0700

    Inisialisasi queue saat kosong

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -23,10 +23,16 @@ class Queues
             cout << endl;
 
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
-            { 
-                cout << "\nQueue overflow\n";
-                return;
-            }
+        { 
+            cout << "\nQueue overflow\n";
+            return;
+        }
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+            
 
  
     }
